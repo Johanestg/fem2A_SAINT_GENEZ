c7e795aa59f7673fbf652071ab51dc8463ba4dc1
Johane <159522849+Johanestg@users.noreply.github.com>
2024-05-12 19:46:24 +0200

    Comments on fem.cpp

--- src/fem.cpp
+++ src/fem.cpp
@@ -134,7 +134,7 @@ namespace FEM2A {
         : border_( border )
     {
         if (border) 
-	// Get the mapping of two vertices, one for each points of an edge 
+	// Get the mapping of two vertices, one for each points of an edge.
         {	
         	for (int nb_ver=0; nb_ver<2; nb_ver++)
         	{
@@ -142,7 +142,7 @@ namespace FEM2A {
         	}
         }
         else 
-	// Get the mapping of three vertices, one for each points of a triangle 
+	// Get the mapping of three vertices, one for each points of a triangle.
         {
         	for (int nb_ver=0; nb_ver<3; nb_ver++)
         	{
@@ -156,11 +156,15 @@ namespace FEM2A {
     {
         vertex r ;
         if (border_) 
+	/* Transforms a point from the reference element
+           edge to the world space. */
         {
 	        r.x = (1 - x_r.x )*vertices_[0].x + x_r.x*vertices_[1].x;
 	        r.y = (1 - x_r.x )*vertices_[0].y + x_r.x*vertices_[1].y;
         }
         else
+	/* Transforms a point from the reference element
+           triangle to the world space. */
         {
         	r.x = (1 - x_r.x - x_r.y)*vertices_[0].x + x_r.x*vertices_[1].x + x_r.y*vertices_[2].x;
 	        r.y = (1 - x_r.x - x_r.y)*vertices_[0].y + x_r.x*vertices_[1].y + x_r.y*vertices_[2].y;
@@ -172,16 +176,18 @@ namespace FEM2A {
     {
         DenseMatrix J ;
         if (border_)
+	// Computes the jacobian matrix of the mapping of an edge.
         {
         	J.set_size(2,1); 
-        	J.set(0, 0, -vertices_[0].x + vertices_[1].x);/* Derivative of the x coordinates of x_r with respect to xie*/
-        	J.set(1, 0, -vertices_[0].y + vertices_[1].y);/* Derivative of the y coordinates of x_r with respect to xiee*/
+        	J.set(0, 0, -vertices_[0].x + vertices_[1].x);/* Derivative of the x coordinates of x_r with respect to xie */
+        	J.set(1, 0, -vertices_[0].y + vertices_[1].y);/* Derivative of the y coordinates of x_r with respect to xie*/
         }
         else
+	// Computes the jacobian matrix of the mapping of a triangle.
         {
         	J.set_size(2,2); 
-        	J.set(0, 0, -vertices_[0].x + vertices_[1].x);
-        	J.set(1, 0, -vertices_[0].y + vertices_[1].y);
+        	J.set(0, 0, -vertices_[0].x + vertices_[1].x);/* Derivative of the x coordinates of x_r with respect to xie */
+        	J.set(1, 0, -vertices_[0].y + vertices_[1].y);/* Derivative of the y coordinates of x_r with respect to xie*/
         	J.set(0, 1, -vertices_[0].x + vertices_[2].x);/* Derivative of the x coordinates of x_r with respect to eta*/
         	J.set(1, 1, -vertices_[0].y + vertices_[2].y);/* Derivative of the y coordinates of x_r with respect to eta*/
         }
@@ -190,30 +196,32 @@ namespace FEM2A {
 
     double ElementMapping::jacobian( vertex x_r ) const
     {
-
-        DenseMatrix J = jacobian_matrix(x_r); // Création de la matrice jacobienne du vertex x_r
-        double det = 0.;
+	// Creation of the jacobian matrix of x_r's vertex.
+        DenseMatrix J = jacobian_matrix(x_r); 
+	    
         if (border_)
+	// Computes the determinant of the jacobian matrix of the mapping of an edge.
         {
-        	det = sqrt( J.get(0,0)*J.get(0,0) + J.get(1,0)*J.get(1,0) );
+        	double det = sqrt( J.get(0,0)*J.get(0,0) + J.get(1,0)*J.get(1,0) );
         }
         else
+	// Computes the determinant of the jacobian matrix of the mapping of a triangle.
         {
-        	det = J.det_2x2(); //Calcul du déterminant
+        	double det = J.det_2x2(); //Calcul du déterminant
         }
         return det ;
     }
-    
-    
-    /** johane **/
-    
+
+   /** 
+   * \brief Get the coordinates of vertices.
+   *        Used to test ElementMapping.
+   */
    int ElementMapping::get_vertices()
    {
    	for (int j=0; j< vertices_.size() ; j++)
    	{
    		std::cout << "v" << j << " : " << vertices_[j].x << "	" << vertices_[j].y << std::endl;
    	}
-   	
    	return 0;
    }
    
@@ -221,9 +229,11 @@ namespace FEM2A {
     /****************************************************************/
     /* Implementation of ShapeFunctions */
     /****************************************************************/
+
     ShapeFunctions::ShapeFunctions( int dim, int order )
         : dim_( dim ), order_( order )
-    {
+    {  
+	// Verification of values for dim_ and order_
         assert( (dim_ ==1) || (dim_ ==2) );
         assert( order_== 1 );
     }
@@ -238,7 +248,6 @@ namespace FEM2A {
 
     double ShapeFunctions::evaluate( int i, vertex x_r ) const
     {
-        
         double shape_func=0.;
         if (dim_==1)
         {
@@ -257,10 +266,8 @@ namespace FEM2A {
 
     vec2 ShapeFunctions::evaluate_grad( int i, vertex x_r ) const
     {
-        //std::cout << "[ShapeFunctions] evaluate gradient shape function " << i << '\n';
-        // TODO
         vec2 g ;
-        
+       
         if (dim_==1)
         {
         	if ( i == 0)
@@ -290,65 +297,58 @@ namespace FEM2A {
         		g.x = 0;
         		g.y = 1;
         	}	
-        }
-        
+        }      
         return g ;
     }
 
     /****************************************************************/
     /* Implementation of Finite Element functions */
     /****************************************************************/
+
     void assemble_elementary_matrix(
-        const ElementMapping& elt_mapping, // un élément : segment ou triangle
-        const ShapeFunctions& reference_functions, /* les shape fonctions associées aux vertex de l'élément */ 
-        const Quadrature& quadrature, // fonction de quadrature
-        double (*coefficient)(vertex), /* coefficient est un pointeur de fonction attendant un paramètre vertex et renvoyant un double*/
-        DenseMatrix& Ke ) // Ke est une référence d'une matrice
+        const ElementMapping& elt_mapping, // an element: a segment or a triangle
+        const ShapeFunctions& reference_functions, /* shape functions associated with vertices of the element */ 
+        const Quadrature& quadrature, 
+        double (*coefficient)(vertex), 
+        DenseMatrix& Ke ) 
     {
-    std::cout << "local matrix in " << std::endl;
-        
-        for (int i=0 ; i < reference_functions.nb_functions(); i++)
+        for (int i=0 ; i < reference_functions.nb_functions(); i++) /*for each shape function*/
         {
         	for (int j=0; j < reference_functions.nb_functions(); j++)
         	{
        			double sum_Ke = 0.;
-        
         		for (int q =0 ; q < quadrature.nb_points() ; q++)
         		{
-        			vertex pt_integration = quadrature.point(q); 
-        			
+        			vertex pt_integration = quadrature.point(q); 		
         			double wq = quadrature.weight(q);
-        			/* Matrice jacobienne, en fonction du point de Gauss regardé ( = point de 		 référence )*/
+				
+        			/* Jacobian matrix with respect to Gauss' points (here they are equivalent to reference points)*/
         			DenseMatrix jacob_mat = elt_mapping.jacobian_matrix( pt_integration );
         			DenseMatrix inv_jacob_mat = jacob_mat.invert_2x2();
         			DenseMatrix trans_inv_jacob_mat = inv_jacob_mat.transpose(); 
         		
-        			/* Gradient de shape function d'indice i (doit le faire 3 fois pour un triangle*/
+        			/* Gradient of shape function of index i*/
         			vec2 grad_shape_func_i = reference_functions.evaluate_grad(i, pt_integration );
         		
-        			/* Gradient de shape function d'indice t (doit le faire 3 fois pour un triangle*/
+        			/* Gradient of shape function of index t*/
         			vec2 grad_shape_func_j = reference_functions.evaluate_grad(j, pt_integration );
         			
-        			/* Détermiant de la matrice jacobienne de l'élément e*/
+        			/* Jacobian matrix determinant of elt_mapping*/
         			double det_jacob_mat = elt_mapping.jacobian( pt_integration );
  
- 				/***************** Somme pour Ke *****************/
+ 				/***************** Sum for Ke *****************/
  				
- 				/* Produit scalaire */   
+ 				/* Scalar product */   
  				vec2 vect_je_grad_i = trans_inv_jacob_mat.mult_2x2_2( grad_shape_func_i );  
  				vec2 vect_je_grad_j = trans_inv_jacob_mat.mult_2x2_2( grad_shape_func_j );
  				
  				double prod_scal = dot( vect_je_grad_i , vect_je_grad_j );
  				
  				sum_Ke += wq * coefficient( pt_integration ) * prod_scal * det_jacob_mat ;
-        		}
-        		
-        		Ke.set( i, j, sum_Ke ); /*Comme on appelle la réf de Ke, Ke est updaté en dehors de cette fonction*/
+        		}	
+        		Ke.set( i, j, sum_Ke ); 
         	}
         }
-        Ke.print();
-        std::cout << "ele matrix out " << std::endl;
-        
     }
 
     void local_to_global_matrix(
@@ -357,24 +357,16 @@ namespace FEM2A {
         const DenseMatrix& Ke,
         SparseMatrix& K )
     {
-    std::cout << "local matrix in" << std::endl;
-    	
         for (int i=0; i<3; i++)
         {
+		int glob_ind1 = M.get_triangle_vertex_index( t, i);
         	for (int j=0; j<3; j++)
         	{ 
-        		int glob_ind1 = M.get_triangle_vertex_index( t, i);
         		int glob_ind2= M.get_triangle_vertex_index( t, j) ;
-        		std::cout << "Je vais ajouter à K un élément de Ke" << std::endl;
-        		std::cout << "glob_ind1= " << glob_ind1<< std::endl;
-        		std::cout << "glob_ind2= " << glob_ind2<< std::endl;
-        		std::cout << "KE " << Ke.get(i,j)<< std::endl;
         		K.add(glob_ind1, glob_ind2, Ke.get(i,j));
-        		std::cout << "Je l'ai ajouté à K un élément de Ke" << std::endl;
         	}
         }
-       	// K.print();  
-       	std::cout << "local matrix out " << std::endl;     
+       	// K.print();  // Undo comment to compute test_local_to_global_matrix()
     }
 
     void assemble_elementary_vector(
@@ -384,8 +376,6 @@ namespace FEM2A {
         double (*source)(vertex),
         std::vector< double >& Fe )
     {
-    std::cout << "ele vector in " << std::endl;
-        
         for (int i=0; i< reference_functions.nb_functions(); i++)
         {
         	double sum_Fe=0.;
@@ -397,17 +387,16 @@ namespace FEM2A {
         		
         		double val_shape_func = reference_functions.evaluate( i, pt_integration );
         		
-        		/* Valeur de f au point de la transformation Me*/
+        		/* Value of f at the point of Me transformation*/
         		double f = source( pt_integration);
         		
-        		/* Détermiant de la matrice jacobienne de l'élément e*/
+        		 Jacobian matrix determinant of elt_mapping*/
         		double det_jacob_mat = elt_mapping.jacobian( pt_integration );
         		
         		sum_Fe += wq * val_shape_func * f * det_jacob_mat;
         	}
         	Fe.push_back( sum_Fe );
     	}
-    	std::cout << "ele vector out " << std::endl;
     }
     
 
@@ -421,7 +410,8 @@ namespace FEM2A {
         for (int i=0; i< reference_functions_1D.nb_functions(); i++)
         {
         	double sum_Fe=0.;
-        	for (int q=0; q < quadrature_1D.nb_points(); q++) /* nombre de point d'intégration est nb de pt quadra*/
+        	for (int q=0; q < quadrature_1D.nb_points(); q++) 
+		/* The number of integration points are the number of quadrature points*/
         	{
         		vertex pt_integration = quadrature_1D.point(q); 
         			
@@ -429,12 +419,10 @@ namespace FEM2A {
         		
         		double val_shape_func = reference_functions_1D.evaluate( i, pt_integration );
         		
-        		/* Valeur de h au point de la transformation Me*/
+        		/* Value of h at the transformation point Me*/
         		double h = neumann( pt_integration);
         		
-        		/* Détermiant de la matrice jacobienne de l'élément e*/
-        		//DenseMatrix matrice_jacobienne = elt_mapping_1D.jacobian_matrix(pt_integration);
-        		//double racine_jacob = sqrt(matrice_jacobienne.transpose() * matrice_jacobienne)
+        		/* Jacobian matrix determinant of elt_mapping_1D*/
         		double racine_jacob = elt_mapping_1D.jacobian( pt_integration);
         		sum_Fe += wq * val_shape_func * h * racine_jacob;
         	}
@@ -450,31 +438,23 @@ namespace FEM2A {
         std::vector< double >& Fe,
         std::vector< double >& F )
     {
-        std::cout << "Fe -> F" << '\n';
-        // TODO
-        
-        std::vector<int> ind_global;
-        
+        std::vector<int> ind_global; 
         if (border)
         {
         	for (int ind_local =0; ind_local < Fe.size(); ind_local++)
         	{
         		F[ M.get_edge_vertex_index( i, ind_local) ] = Fe[ind_local];
-        		
         		//std::cout << "le numéro global du point " << ind_local << " est " << M.get_edge_vertex_index( i, ind_local) << "." << std::endl;
         	}
         }
-        
         else
         {
-        std::cout << "local vector in " << std::endl;
         	for (int ind_local =0; ind_local < Fe.size(); ind_local++)
         	{
         		F[ M.get_triangle_vertex_index( i, ind_local) ] = Fe[ind_local];
         		//std::cout << "le numéro global du point " << ind_local << " est " << M.get_triangle_vertex_index( i, ind_local) << "." << std::endl;
         	}
         }
-        std::cout << "local vector out " << std::endl;
     }
 
     void apply_dirichlet_boundary_conditions(
@@ -484,36 +464,34 @@ namespace FEM2A {
         SparseMatrix& K,
         std::vector< double >& F )
     {
-        std::cout << "apply dirichlet boundary conditions" << '\n';
-        // TODO
-        /*vecteur taille de values rempli de false*/
+        /*vector of size of values filled with false*/
        	std::vector<bool> processed_vertices(values.size(), false); 
-       	/* penalty_coefficient est le gros coefficient P*/
+       	/* penalty_coefficient is a big coefficient*/
         double penalty_coefficient =10000.;
         
-        /* On parcourt tous les edge du bord de la simulation*/
         for (int i=0; i< M.nb_edges(); i++)
         {
-        	/* Pour chaque edge, on récupère la valeur de son de son attribut*/
+        	/* For each edge, we get the values of the attribute*/
         	int edge_attribute = M.get_edge_attribute(i);
         	
         	/* 
-        	Dans attribut_is_diriclet, il est stocké la correspondance entre la valeur d'un attribut d'un edge et
-        	le fait que cet edge est un bord du maillage ou pas. True si c'est un bord, false sinon
-        	si c'est tru, on rentre dans le if */
+        	In attribut_is_diriclet, there is the information about if it's an edge or not.
+	 	True if it's an edge, false otherwise.
+        	 */
         	if (attribute_is_dirichlet[edge_attribute])
         	{
-        	/* Pour chacun des vertices associés au edge du bord*/
-        	for( int v = 0; v < 2; v++ ) {
-                    int vertex_index = M.get_edge_vertex_index(i, v);
-                    if( !processed_vertices[vertex_index] ) {
-                        processed_vertices[vertex_index] = true;
-                        K.add(vertex_index, vertex_index, penalty_coefficient);
-                        F[vertex_index] += penalty_coefficient*values[vertex_index]; /* values correspond à g*/
-                    	}
+        	/* For each vertices associated with the edge*/
+        	    for( int v = 0; v < 2; v++ ) 
+		    {
+                        int vertex_index = M.get_edge_vertex_index(i, v);
+                        if( !processed_vertices[vertex_index] ) 
+		        {
+                            processed_vertices[vertex_index] = true;
+                            K.add(vertex_index, vertex_index, penalty_coefficient);
+                            F[vertex_index] += penalty_coefficient*values[vertex_index]; /* values correspond à g*/
+                        }  
                     }
-
-        	}
+               }
         }
     }
 
