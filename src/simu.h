b84d05076cf75b6dc0b7fae0c9ba8f0a2361464b
Johane <159522849+Johanestg@users.noreply.github.com>
2024-05-15 23:11:31 +0200

    enlever fine dans nom pure Dirichlet simu.h

--- src/simu.h
+++ src/simu.h
@@ -90,7 +90,7 @@ namespace FEM2A {
 	    /*** Résolution du système ***/
 	    std::vector< double > x(M.nb_vertices());
 	    solve( K, F, x );
-	    std::string out_name = "pb_pure_dirichlet_fine";
+	    std::string out_name = "pb_pure_dirichlet";
 	    M.save("./output/"+out_name+".mesh");  /* créer un dossier simulation où stocker les mesh et la solution des simulation*/
 	    save_solution(x, "./output/"+out_name+".bb");
 
